dfc5711e227ff241c9dc2b74f913f12d988b27ad
Rajkiran Rajkumar <rajkiran.rajkumar@personio.de>
2023-11-09 10:15:30 +0100

    feat: introduce enum for cell state; replace duplicated usages

--- kotlin/junit/src/test/kotlin/com/kata/GameOfLifeTest.kt
+++ kotlin/junit/src/test/kotlin/com/kata/GameOfLifeTest.kt
@@ -1,32 +1,38 @@
 package com.kata
 
+import com.kata.CellState.DEAD
+import com.kata.CellState.LIVE
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+
+enum class CellState {
+    LIVE, DEAD
+}
 
 internal class GameOfLifeTest {
     @Test
     fun testCellState_Live() {
-        val cellState = "LIVE"
-        assertEquals(cellState, "LIVE")
+        val cellState = LIVE
+        assertEquals(cellState, LIVE)
     }
 
     @Test
     fun testCellState_Dead() {
-        val cellState = "DEAD"
-        assertEquals(cellState, "DEAD")
+        val cellState = DEAD
+        assertEquals(cellState, DEAD)
     }
 
     @Test
     fun testCellChangeState_LiveDead() {
-        var cellState = "LIVE"
-        cellState = "DEAD"
-        assertEquals(cellState, "DEAD")
+        var cellState = LIVE
+        cellState = DEAD
+        assertEquals(cellState, DEAD)
     }
 
     @Test
     fun testCellChangeState_DeadLive() {
-        var cellState = "DEAD"
-        cellState = "LIVE"
-        assertEquals(cellState, "LIVE")
+        var cellState = DEAD
+        cellState = LIVE
+        assertEquals(cellState, LIVE)
     }
 }
