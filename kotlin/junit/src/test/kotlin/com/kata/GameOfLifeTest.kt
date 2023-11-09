000b3e5969c10d66655bf238df91aa0521b63933
Nikola Marić <nikola.maric@personio.de>
2023-11-09 10:11:54 +0100

    feat: test state DEAD; test state transitions

--- kotlin/junit/src/test/kotlin/com/kata/GameOfLifeTest.kt
+++ kotlin/junit/src/test/kotlin/com/kata/GameOfLifeTest.kt
@@ -5,9 +5,28 @@ import org.junit.jupiter.api.Test
 
 internal class GameOfLifeTest {
     @Test
-    fun testCellState() {
+    fun testCellState_Live() {
         val cellState = "LIVE"
         assertEquals(cellState, "LIVE")
     }
-}
 
+    @Test
+    fun testCellState_Dead() {
+        val cellState = "DEAD"
+        assertEquals(cellState, "DEAD")
+    }
+
+    @Test
+    fun testCellChangeState_LiveDead() {
+        var cellState = "LIVE"
+        cellState = "DEAD"
+        assertEquals(cellState, "DEAD")
+    }
+
+    @Test
+    fun testCellChangeState_DeadLive() {
+        var cellState = "DEAD"
+        cellState = "LIVE"
+        assertEquals(cellState, "LIVE")
+    }
+}
